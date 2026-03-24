404625204bea47f21f828744b950ca981f2c3f8e
Wahab Ismail Adewale <wismailadewale@gmail.com>
2026-03-24 22:56:42 +0100

    fix blueprint url_for dashboard

--- auth.py
+++ auth.py
@@ -14,7 +14,7 @@ def login():
         # FIXED CREDENTIALS
         if username == "admin" and password == "Alayinde001":
             session["user"] = username
-            return redirect(url_for("weighbridge.dashboard"))
+            return redirect(url_for("weighbridge_bp.dashboard"))
         else:
             return render_template("login.html", error="Invalid login details")
 
