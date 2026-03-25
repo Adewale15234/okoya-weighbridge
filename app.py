2b2fde385105a775c089740866005e0b00a2b881
Wahab Ismail Adewale <wismailadewale@gmail.com>
2026-03-25 22:50:38 +0100

    Connected PostgreSQL database

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
+import os
 from flask import Flask
 from models import db
 
 app = Flask(__name__)
 app.secret_key = "supersecretkey"
 
-# DATABASE CONFIG
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+# DATABASE CONFIG (FIXED)
+database_url = os.environ.get("DATABASE_URL")
+
+# Fix for Render PostgreSQL (important)
+if database_url:
+    database_url = database_url.replace("postgres://", "postgresql://")
+
+app.config['SQLALCHEMY_DATABASE_URI'] = database_url or 'sqlite:///database.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db.init_app(app)
@@ -17,7 +24,7 @@ from weighbridge import weighbridge_bp
 app.register_blueprint(auth_bp)
 app.register_blueprint(weighbridge_bp, url_prefix="/weighbridge")
 
-# CREATE DB SAFELY
+# CREATE DB
 with app.app_context():
     db.create_all()
 
